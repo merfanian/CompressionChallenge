82789a03698cf0baf166d3900fc1ee983f3f44e6
mahdi <erfanianmahdi00@gmail.com>
2019-05-11 21:30:43 +0430

    Add initial classes.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hi, type your command num:");
+        System.out.println("1. compress file");
+        System.out.println("2. decompress file");
+        int i = scanner.nextInt();
+    }
+}
